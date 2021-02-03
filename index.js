ec184ed2b9130d5a5d8d59021e2f373093821b87
wdfhai <wd.fhai@outlook.com>
2021-02-03 15:03:03 -0600

    added greeting and goodbye messages

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const fs = require('fs');
 const inquirer = require ('inquirer');
-const enquirer = require('enquirer');
 const chalk = require ('chalk');
 const emoji = require ('node-emoji');
 const questions = require ('./questions');
-
-console.clear();
+const template = require ('./template');
 
 const greeting = 
 `
 
-                        ${emoji.get('file_folder')}  ${chalk.bgMagenta('Welcome to the Dream Team Builder!')}  ${(emoji.get('file_folder'))}  
+                ${emoji.get('male-technologist')}  ${chalk.bgMagenta('Welcome to the Dev Team Builder!')}  ${(emoji.get('female-technologist'))}  
 
-        ${chalk.cyan('This application will generate a HTML file presenting your team and its members along with their roles and details.')}
+${chalk.cyan('This application will generate a HTML file presenting your team and its members along with their roles and details.')}
 
 `;
 
+const errorDetected = 
+`
+
+${emoji.get('exclamation')} ${chalk.red('Looks like there was an error creating your file. Please address the error or report it, and restart the application')}${emoji.get('exclamation')}
+
+`;
+
+const goodbye = 
+`
+
+${emoji.get('white_check_mark')} ${chalk.blueBright('Dream Team assembled. Your HTML file was successfully written. Check it out. Cheers!')} ${emoji.get('white_check_mark')}
+`;
+
+
+
+console.clear();
+console.log(greeting);
 
 async function intro() {
     try {
@@ -24,11 +39,15 @@ async function intro() {
         if (response.add_more){
             intro();
         } else {
+            const html = template(response);   
+            fs.writeFileSync('./index.html', html);
+            console.log(goodbye);
             return;
         }
+
     } catch (error) {
         console.log(error);
-        console.log('Whoopsie daisy. Error detected.');
+        console.log(errorDetected);
     }
 };
 
